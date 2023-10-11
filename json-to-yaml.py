a9a437b3534e450a55bcc19e517b30bbc8a2b2fb
Jason <46580139+jalacloud@users.noreply.github.com>
2023-10-11 15:25:58 +1100

    Update json-to-yaml.py
    Formatting

--- json-to-yaml.py
+++ json-to-yaml.py
@@ -4,10 +4,10 @@ import json
 import yaml
 
 # Load JSON data from a file
-with open('C:/Users/laytoja/OneDrive - EY/Documents/Projects/ASS3T/enterprise-attack.json', 'r') as json_file:
+with open('{{ INPUT_FILEPATH }}', 'r') as json_file:
     data = json.load(json_file)
 
 # Convert JSON data to YAML
-with open('C:/Users/laytoja/OneDrive - EY/Documents/Projects/ASS3T/enterprise-attack-converted.yaml', 'w') as yaml_file:
+with open('{{ OUTPUT_FILEPATH }}', 'w') as yaml_file:
     yaml.dump(data, yaml_file, default_flow_style=False)
 
